e879c2408974dd026d5f363e936bca78f91e498c
DangHuuGiau <154773475+DangHuuGiau@users.noreply.github.com>
2025-04-16 17:19:28 +0700

    update SEO

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://vinhome-quan9.info.vn", // domain chính của bạn
+  generateRobotsTxt: true, // tạo luôn robots.txt
+  changefreq: "daily",
+  priority: 0.7,
+  sitemapSize: 7000,
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./sections/Header";
 import Footer from "./sections/Footer";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,41 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="robots" content="index, follow" />
+        <meta name="author" content="Vinhomes Grand Park" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          property="og:title"
+          content="Vinhomes Grand Park - Đại đô thị đẳng cấp quốc tế"
+        />
+        <meta
+          property="og:description"
+          content="Vinhomes Grand Park tọa lạc tại vị trí mặt tiền đường Nguyễn Xiển, phường Long Thạnh Mỹ, Quận 9, Tp Thủ Đức, TP Hồ Chí Minh."
+        />
+        <meta property="og:url" content="https://vinhome-quan9.info.vn/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/og-image.jpg" />{" "}
+        <meta name="twitter:card" content="summary_large_image" />
+        <link rel="canonical" href="https://vinhome-quan9.info.vn/" />
+        <link rel="icon" href="/favicon.ico" />
+        <script type="application/ld+json">
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            name: "Vinhomes Grand Park",
+            url: "https://vinhome-quan9.info.vn",
+            logo: "https://vinhome-quan9.info.vn/_next/image?url=%2Fimages%2Fvinhomes-gp-logo.png&w=256&q=75",
+            contactPoint: {
+              "@type": "ContactPoint",
+              telephone: "+84-0375-515-642",
+              contactType: "sales",
+            },
+          })}
+        </script>
+      </Head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
