caa7198564ad95df0c19c51fcf3a1f8f65919e7b
DangHuuGiau <154773475+DangHuuGiau@users.noreply.github.com>
2025-04-18 10:47:15 +0700

    update ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -92,6 +92,20 @@ export default function RootLayout({
             ],
           })}
         </script>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17016219934"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17016219934');
+            `,
+          }}
+        ></script>
       </Head>
 
       <body
